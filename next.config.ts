638ec0eeb7bec83105b70e6b35ecdeaf4d580051
vanthang559 <vanthang23032001@gmail.com>
2024-11-21 20:45:00 +0700

    chore: config hostname

--- next.config.ts
+++ next.config.ts
@@ -8,7 +8,20 @@ const nextConfig: NextConfig = {
         hostname: 'placehold.co',
         port: '',
         pathname: '/**',
-      }
+      },
+      {
+        protocol: 'https',
+        hostname: 'amaranth-calm-hippopotamus-262.mypinata.cloud',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'coral-junior-magpie-341.mypinata.cloud',
+        port: '',
+        pathname: '/**',
+      },
+
     ],
   }
 };
